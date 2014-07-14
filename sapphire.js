a9ec20ba81ec12777ef517a1f43b7118182ebfc0
Callum Smith <callum1234moo@gmail.com>
2014-07-13 20:04:25 -0400

    init

---
+++ sapphire.js
@@ -0,0 +1,66 @@
+testo = {'a':'aa','b':'bb'};
+testo2 = {'a':'aa','b':'bb','c':['a','b','c']};
+testa = ['a','b','c','d'];
+
+Object.prototype.each = function(callback){//iterator for objects or arrays
+	if (Array.isArray(this) || this.toString() === "[object HTMLCollection]") {//HTMLCollection uses the same style methods as array
+		for (var i = 0; i < this.length; i++) {
+			callback(this[i]);//calls the callback once for each element in the array
+		}
+	} else {
+		for (var i = 0; i < Object.keys(this).length; i++) {
+			var k = Object.keys(this)[i];//key
+			var v = this[Object.keys(this)[i]];//value
+			callback(k, v);//calls the callback once for each key. first argument is the key, second argument is the value
+		};
+	}
+	return this;
+}
+
+Object.defineProperty(Object.prototype, "last", {//returns the last value of an object/last element in an array
+	get: function(){
+		if (this.toString() === "[object Object]") {
+			var k = Object.keys(this);
+			var p = k.length - 1;
+			return this[k[p]];
+		} else {
+			var p = this.length - 1;
+			return this[p];
+		}
+	}
+});
+
+Object.defineProperty(Object.prototype, "first", {//returns the first value of an object/first element in an array
+	get: function(){
+		if (this.toString() === "[object Object]") {
+			var k = Object.keys(this);
+			return this[k[0]];
+		} else {
+			return this[0];
+		}
+	}
+});
+
+/*Browser speficic code. Stuff that requires a window or dom*/
+if (window) {
+	window.c = function(e){
+		return document.getElementsByClassName(e);
+	}
+
+	window.t = function(e){
+		return document.getElementsByTagName(e);
+	}
+
+	window.e = window.i = function(e){
+		return document.getElementById(e);
+	}
+
+	window.n = function(){
+		return document.getElementsByName(e);
+	}
+}
+
+Object.defineProperty(Node.prototype, "innerText", {
+	get: function(){ return this.textContent },
+	set: function(t){ this.textContent = t; }
+});
