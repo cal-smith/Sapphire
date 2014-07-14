7cddc2083b21fb307a93f51657743cd59504a118
Callum Smith <callum1234moo@gmail.com>
2014-07-14 00:14:35 -0400

    Mild re-factor

--- sapphire.js
+++ sapphire.js
@@ -64,17 +64,6 @@ Object.prototype.contains = function(search){//checks if objects and arrays cont
 * Objects
 *
 */
-Object.defineProperty(Object.prototype, "values", {
-	get: function(){
-		var array = [];
-		for (var i = 0; i < Object.keys(this).length; i++) {
-			var v = this[Object.keys(this)[i]];
-			array.push(v);
-		}
-		return array;
-	}
-});
-
 Sapphire.o_findkv = function(ctx, value, ret_true){//reduce duplication. takes a ctx(this) the value, and weather to return true or simply the value
 	for (var i = 0; i < Object.keys(ctx).length; i++) {
 		var k = Object.keys(ctx)[i];//key
@@ -90,13 +79,24 @@ Sapphire.o_findkv = function(ctx, value, ret_true){//reduce duplication. takes a
 	return false;
 };
 
-Object.prototype.key = function(value){//returns the key for a given value
-	return Sapphire.o_findkv(this, value, false);
+Object.prototype.key = function(val){//returns the key for a given value
+	return Sapphire.o_findkv(this, val, false);
 };
 
 Object.prototype.val = function(val){//returns true if there exists a given value
 	return Sapphire.o_findkv(this, val, true);
 };
+
+Object.defineProperty(Object.prototype, "values", {
+	get: function(){
+		var array = [];
+		for (var i = 0; i < Object.keys(this).length; i++) {
+			var v = this[Object.keys(this)[i]];
+			array.push(v);
+		}
+		return array;
+	}
+});
 
 /*
 *
