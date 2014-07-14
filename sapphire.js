68c2d348eb3b4b9ef02294a7f8e4f7faa0fd0108
Callum Smith <callum1234moo@gmail.com>
2014-07-13 23:15:48 -0400

    added a lot of fn's & organized code
    added:
    contains
    key
    value(object)
    value(array)
    values
    on
    off
    sleep

--- sapphire.js
+++ sapphire.js
@@ -2,6 +2,11 @@ var testo = {'a':'aa','b':'bb'};
 var testo2 = {'a':'aa','b':'bb','c':['a','b','c']};
 var testa = ['a','b','c','d'];
 
+/*
+*
+* Arrays + Objects
+*
+*/
 Object.prototype.each = function(callback){//iterator for objects or arrays
 	if (Array.isArray(this) || this.toString() === "[object HTMLCollection]") {//HTMLCollection uses the same style methods as array
 		for (var i = 0; i < this.length; i++) {
@@ -41,26 +46,110 @@ Object.defineProperty(Object.prototype, "first", {//returns the first value of a
 	}
 });
 
+Object.prototype.contains = function(search){//checks if objects and arrays contain the search term. returns true/false.
+	if (this.toString() === "[object Object]") {
+		return this.hasOwnProperty(search);
+	} else{
+		if (this.indexOf(search) !== -1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+};
+
+/*
+*
+* Objects
+*
+*/
+Object.prototype.key = function(value){//returns the key for a given value
+	for (var i = 0; i < Object.keys(this).length; i++) {
+		var k = Object.keys(this)[i];//key
+		var v = this[Object.keys(this)[i]];//value
+		if (v === value){
+			return k;
+		}
+	}
+	return false;
+};
+
+Object.defineProperty(Object.prototype, "values", {
+	get: function(){
+		var array = [];
+		for (var i = 0; i < Object.keys(this).length; i++) {
+			var v = this[Object.keys(this)[i]];
+			array.push(v);
+		}
+		return array;
+	}
+});
+
+Object.prototype.value = function(value){//returns true if there exists a given value
+	for (var i = 0; i < Object.keys(this).length; i++) {
+		var k = Object.keys(this)[i];//key
+		var v = this[Object.keys(this)[i]];//value
+		if (v === value){
+			return true;
+		}
+	}
+	return false;
+};
+
+/*
+*
+* Arrays
+*
+*/
+Array.prototype.value = function(value){//returns the position of a given value, or false
+	if (this.indexOf(value) !== -1){
+		return this.indexOf(value);
+	} else {
+		return false;
+	}
+}
+
+//borrowing a little from backbone.js here. .on and .off are just so succinct.
+Object.prototype.on = function(type, callback, capture){
+	this.addEventListener(type, callback, capture);
+	return this;
+};
+
+Object.prototype.off = function(type, callback, capture){
+	this.removeEventListener(type, callback, capture);
+	return this;
+};
+
 /*Browser speficic code. Stuff that requires a window or dom*/
 if (window) {
-	window.c = function(e){
+	window.c = function(e){//gets an element array by class
 		return document.getElementsByClassName(e);
 	};
 
-	window.t = function(e){
+	window.t = function(e){//gets element array by tag
 		return document.getElementsByTagName(e);
 	};
 
-	window.e = window.i = function(e){
+	window.e = window.i = function(e){//gets element by id
 		return document.getElementById(e);
 	};
 
-	window.n = function(e){
+	window.n = function(e){//gets element array by name attribute
 		return document.getElementsByName(e);
 	};
 }
 
-Object.defineProperty(Node.prototype, "innerText", {
-	get: function(){ return this.textContent },
-	set: function(t){ this.textContent = t; }
+window.sleep = function(callback, delay){//wraps setTimeout, and defaults to sleeping in seconds rather than miliseconds.
+	delay = delay * 1000;
+	return window.setTimeout(callback, delay);
+}
+
+document.on("load", function(){
+	//innerHTML and innerText just go together. textContent is cool, but innerText is where it's at.
+	if (typeof document.body.innerText === "undefined") {
+		Object.defineProperty(Node.prototype, "innerText", {
+			get: function(){ return this.textContent; },
+			set: function(t){ this.textContent = t; }
+		});
+	}
 });
